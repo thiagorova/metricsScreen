bfa1f9702772e5b202701b12278a9dba0a1b57b1
ribas9521 <guilherme.rifer@gmail.com>
2016-11-01 17:43:12 -0200

    changes in the validation

--- js/create.js
+++ js/create.js
@@ -1,6 +1,5 @@
-angular.module('metricsApp').controller('createController', function($scope){
+angular.module('metricsApp').controller('createController', function($scope, $location){
   var metrics = new Metrics("eJwNyDsOgDAMBNETgTZre+10XAVSICQ+9++INHrFELAQmEF3V/OiuhhtPqUbyAT6wmpUWc7Qw8mqtGaQDBMVIoy2jO+493c7n/261/E9P8cBFlU=");
-
   $scope.period = 'For when is your project';
   $scope.datepickerShow = 'false';
   $scope.project = {
@@ -11,8 +10,7 @@ angular.module('metricsApp').controller('createController', function($scope){
     'deadline':''
     };
   $scope.projectsList = [];
-
-  //This is a watcher function, which changes the milestone parameter based on the select field
+    //This is a watcher function, which changes the milestone parameter based on the select field
   $scope.$watch('project.selectMilestone', function() {
     if($scope.project.selectMilestone == 'wDay'){
       $scope.period = 'How many words do you want to write per day';
@@ -26,66 +24,75 @@ angular.module('metricsApp').controller('createController', function($scope){
     }
   });
 
-  $scope.validationMaster = true;
-  $scope.validationPjName = function(){
-    if($scope.project.projectName.length <= 0) {
-      $scope.pjNameShow = true;
-      return true;
-    } else{
-      $scope.pjNameShow = false;
+  function dateValidation(date){
+    today = new Date();
+    if(date < today){
       return false;
     }
-  }
-
-  $scope.validationTotalWords = function(){
-    if($scope.project.totalWords <= 0 || $scope.project.totalWords === null){
-      $scope.tWordsShow = true;
+    else {
       return true;
-    } else{
-      $scope.tWordsShow = false;
+    }
+  }
+  function nameValidation(name){
+    if(name.length <=0){
       return false;
     }
-  }
-
-  $scope.validationMeasure = function(){
-    if($scope.project.selectMilestone === "deadline") return false;
-    if($scope.project.milestoneMeasure <= 0 ){
-      $scope.measureShow = true;
+    else {
       return true;
-    } else{
-      $scope.measureShow = false;
+    }
+  }
+  function numberValidation(number){
+    if(number <= 0){
       return false;
     }
-  }
-
-  $scope.validationDate = function(){
-    if($scope.project.selectMilestone === "wMonth" || $scope.project.selectMilestone === "wDay") return false;
-    return false;
-  }
-
-  $scope.validationMType = function(){
-    if($scope.project.selectMilestone === "none")
-      $scope.validationMaster = true;
-    else
-      $scope.validationMaster = false;
-  }
-
-  $scope.unifiedValidation = function(){
-    if ($scope.validationPjName() ||
-    $scope.validationTotalWords() ||
-    $scope.validationMeasure() ||
-    $scope.validationDate() ||
-    $scope.validationMType()) {
-       $scope.validationMaster = true;
-    } else {
-      $scope.validationMaster = false;
+    else {
+      return true;
     }
   }
+  $scope.disable = function(){
+    switch ($scope.datepickerShow) {
+      case true:
+        if(dateValidation($scope.project.deadline) &&
+            numberValidation($scope.project.totalWords)
+          && nameValidation($scope.project.projectName)){
+          return false;
+        }
+        else {
+          return true;
+        }
+        break;
+      case false:
+        if(numberValidation($scope.project.totalWords) &&
+         numberValidation($scope.project.milestoneMeasure)
+          && nameValidation($scope.project.projectName)){
+          return false;
+        }
+        else {
+          return true;
+        }
+        break;
+      default:
+        if(dateValidation($scope.project.deadline) &&
+            numberValidation($scope.project.totalWords)
+          && nameValidation($scope.project.projectName)){
+          return false;
+        }
+        else {
+          return true;
+        }
+    }
+  };
    //function to create a project
   $scope.addProject = function(){
     var milestoneMeasure = $scope.project.milestoneMeasure;
     if  (!milestoneMeasure) milestoneMeasure = $scope.project.deadline;
     metrics.createProject($scope.project.projectName, $scope.project.totalWords, $scope.project.selectMilestone, milestoneMeasure);
     $scope.project = {};
+    $location.path('/projects');
   };
+  
+  $scope.$watch('project', function(){
+    $scope.disable();
+  }, true);
+
 });
