906b5f196f8d14c0fd917c564679b4e72527e2f2
Thiago <thiagorova@gmail.com>
2016-11-24 13:11:46 -0200

    fixing minor equality check error

--- js/projects.js
+++ js/projects.js
@@ -115,7 +115,7 @@ angular.module('metricsApp').controller('projectsController', function($scope, $
   $scope.setSystem = function () {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {request: "getId"}, function (projectId) {
-      if(projectId === null) {
+      if(projectId == null) {
         $scope.showProjects()
       } else {
         $scope.metrics.getProject(projectId, GoToProject,
