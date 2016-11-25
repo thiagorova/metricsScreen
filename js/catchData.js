e60c5c01ec8f0dd046571d1164109d8986636d6e
Thiago <thiagorova@gmail.com>
2016-11-25 09:51:02 -0200

    removing useless stuff and resetting some style details

--- js/catchData.js
+++ js/catchData.js
@@ -1,1 +1,18 @@
-var text=document.createElement("textarea");text.style.display="none",text.id="myReallyFakeTextArea";var body=document.getElementsByTagName("body");if(body[0].appendChild(text),"undefined"!=typeof window.tinyMCE)for(var len=window.tinyMCE.editors.length,editors=window.tinyMCE.editors,i=0;len>i;i+=1)text.value=editors[i].getContent({format:"text"});else text.value="";
+var text=document.createElement("textarea");
+text.style.display="none";
+text.id="myReallyFakeTextArea";
+var body=document.getElementsByTagName("body");
+body[0].appendChild(text);
+console.log(window.tinyMCE);
+if(typeof window.tinyMCE !== "undefined") {
+  var len=window.tinyMCE.editors.length;
+  console.log(len);
+  var editors=window.tinyMCE.editors;
+  console.log(editors);
+  for(var i = 0; len > i; i++) {
+    text.value += editors[i].getContent({format:"text"});
+      console.log(text.value);
+  }
+} else { 
+  text.value="";
+}
