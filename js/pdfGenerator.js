5ebbbbff6d0fba7e6645d5bc649aa2d8eba4c008
GUilherme Ribas <GUilherme Ribas>
2017-01-31 20:20:42 -0200

    nothing

--- js/pdfGenerator.js
+++ js/pdfGenerator.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
   $( "#export" ).click(function() {
-    if(dProject !== null){
+
       var y = 70;
-      var projectName = dProject.projectName;
+
       var doc = new jsPDF();
-      doc.text(105, 20, 'Production of: ' + projectName, null, null, 'center');
+      doc.text(105, 20, 'Production', null, null, 'center');
       doc.text(20, 50, 'Date');
       doc.text(105, 50, 'Words');
       doc.text(170, 50, 'Hours');
@@ -15,10 +15,7 @@ $( document ).ready(function() {
         doc.text(170, y, exportData[i].time.toString());
         y += 20;
       }
-      doc.save('Production - ' + projectName + '.pdf');
-    }
-    else {
-      alert('undefined project');
-    }
+      doc.save('Production.pdf');
+  
   });
 });
