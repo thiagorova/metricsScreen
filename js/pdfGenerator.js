9d6575d500dcb370947f9c88c1f99696073037c7
GUilherme Ribas <GUilherme Ribas>
2017-01-31 19:48:57 -0200

    Export to pdf done, but still trying to resolve a bug

--- js/pdfGenerator.js
+++ js/pdfGenerator.js
@@ -1,13 +1,20 @@
 $( document ).ready(function() {
   $( "#export" ).click(function() {
-    if(dProject){
-      console.log(dProject);
+    if(dProject !== null){
+      var y = 70;
       var projectName = dProject.projectName;
       var doc = new jsPDF();
       doc.text(105, 20, 'Production of: ' + projectName, null, null, 'center');
       doc.text(20, 50, 'Date');
-      doc.text(90, 50, 'Words');
+      doc.text(105, 50, 'Words');
       doc.text(170, 50, 'Hours');
+      var i;
+      for(i = 0; i < exportData.length; i++){
+        doc.text(20, y, exportData[i].day.toString());
+        doc.text(110, y,''+ exportData[i].words.toString());
+        doc.text(170, y, exportData[i].time.toString());
+        y += 20;
+      }
       doc.save('Production - ' + projectName + '.pdf');
     }
     else {
