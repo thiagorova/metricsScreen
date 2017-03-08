4dab57a0b6cb041806af1bca2ce0cec8a2cf1478
Guilherme <guilherme.rifer@gmail.com>
2017-03-08 13:37:02 -0300

    translations system working, project page translated to portuguese and ready to be translated to germany

--- js/translation.js
+++ js/translation.js
@@ -1,28 +1,29 @@
 $( document ).ready(function() {
   var language  = 'en';
   var pageName = location.href.split("/").slice(-1)[0].split(".").slice(-2)[0];
-  var xml = '';
+
 
   $( "#lEn" ).click(function() {
     language = 'en';
+    openXml(pageName, language);
   });
   $( "#lDe" ).click(function() {
     language = 'de';
-
+    openXml(pageName, language);
   });
   $( "#lPt" ).click(function() {
     language = 'pt';
+    openXml(pageName, language);
   });
-  openXml(pageName);
 
-
-
-  function openXml(pageName){
+  function openXml(pageName, language){
     var xhttp = new XMLHttpRequest();
+    xhttp.overrideMimeType('text/xml');
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      this.xml = this.responseText;
-      translate(this.xml, this.language);
+      var parser = new DOMParser();
+      var xmlDoc = parser.parseFromString(this.responseText, "application/xml");
+      translate(xmlDoc, language);
     }
   };
   xhttp.open("GET", "xml/" + pageName + ".xml", true);
@@ -31,9 +32,16 @@ $( document ).ready(function() {
 
   function translate(xml, language){
     console.log(xml);
-    console.log(language);
+    var idiom = xml.getElementsByTagName(language)[0];
+    var i = 0;
+    idiomLength = idiom.children.length;
+
+    for(i=0; i< idiomLength; i++){
+      if(document.getElementById(idiom.children[i].tagName)){
+      document.getElementById(idiom.children[i].tagName).innerHTML = idiom.children[i].innerHTML;
+      console.log(idiom.children[i].innerHTML);
+      }
+    }
   }
-
-
 
 });
